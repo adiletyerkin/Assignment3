ba08a42f9e09085b67b94f0372e67ec85e5638dc
Adilet Yerkin <adilet.yerkin@gmail.com>
2021-02-26 20:07:52 +0600

    after creation components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,16 @@ import { HeaderComponent } from './header/header.component';
 import { MainContainerComponent } from './main-container/main-container.component';
 import { CreatedProductsComponent } from './main-container/created-products/created-products.component';
 import { CreatProductsComponent } from './main-container/creat-products/creat-products.component';
+import { ProductsComponent } from './products/products.component';
+import { NoveltiesComponent } from './products/novelties/novelties.component';
+import { GiftsComponent } from './products/gifts/gifts.component';
+import { AboutComponent } from './about/about.component';
+import { OfficialsComponent } from './about/officials/officials.component';
+import { AwardsComponent } from './about/awards/awards.component';
+import { NewsComponent } from './news/news.component';
+import { AddressesComponent } from './addresses/addresses.component';
+import { BrandShopsComponent } from './addresses/brand-shops/brand-shops.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +29,17 @@ import { CreatProductsComponent } from './main-container/creat-products/creat-pr
     HeaderComponent,
     MainContainerComponent,
     CreatedProductsComponent,
-    CreatProductsComponent
+    CreatProductsComponent,
+    ProductsComponent,
+    NoveltiesComponent,
+    GiftsComponent,
+    AboutComponent,
+    OfficialsComponent,
+    AwardsComponent,
+    NewsComponent,
+    AddressesComponent,
+    BrandShopsComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
