eaaac8cdf08519efeef715de4e26a94aa4c88417
Adilet Yerkin <adilet.yerkin@gmail.com>
2021-02-26 20:23:32 +0600

    after adding in app route routes

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -5,17 +5,37 @@ import { HeaderComponent } from './header/header.component';
 import { MainContainerComponent } from './main-container/main-container.component';
 import { CreatedProductsComponent } from './main-container/created-products/created-products.component';
 import { CreatProductsComponent } from './main-container/creat-products/creat-products.component';
+import { ProductsComponent } from './products/products.component';
+import { NoveltiesComponent } from './products/novelties/novelties.component';
+import { GiftsComponent } from './products/gifts/gifts.component';
+import { AboutComponent } from './about/about.component';
+import { OfficialsComponent } from './about/officials/officials.component';
+import { AwardsComponent } from './about/awards/awards.component';
+import { NewsComponent } from './news/news.component';
+import { AddressesComponent } from './addresses/addresses.component';
+import { BrandShopsComponent } from './addresses/brand-shops/brand-shops.component';
+import { HistoryComponent } from './history/history.component';
 
 export const routes: Routes = [
-  // {path: 'home', component: HomeComponent},
-  // {path: 'user', component: UserComponent,
-  //   canActivate: [UserGuardService],
-  //   children: [
-  //     {path: 'detail/:id', component: UserDetailComponent}
-  //   ]},
-  // {path: 'contact', component: ContactComponent},
-  // {path: 'old-product', component: ProductComponent},
-  // {path: 'new-product', loadChildren: './new-product/new-product.module#NewProductModule' },
-  // {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'ProductsComponent', component: ProductsComponent,
+    canActivate: [NoveltiesComponent, GiftsComponent],
+  children: [
+    {path: 'NoveltiesComponent', component: NoveltiesComponent},
+    {path: 'GiftsComponent', component: GiftsComponent}
+  ]},
+  {path: 'AboutComponent', component: AboutComponent,
+  canActivate: [OfficialsComponent, AwardsComponent],
+  children: [
+    {path: 'OfficialsComponent', component: OfficialsComponent},
+    {path: 'AwardsComponent', component: AwardsComponent}
+  ]},
+  {path: 'NewsComponent', component: NewsComponent},
+  {path: 'AddressesComponent', component: AddressesComponent,
+  canActivate : [BrandShopsComponent],
+  children: [
+    {path: 'BrandShopsComponent', component: BrandShopsComponent},
+  ]},
+  {path: 'HistoryComponent', component: HistoryComponent},
+  {path: '', redirectTo: '/', pathMatch: 'full'},
   // {path: '**', component: ErrorComponent}
 ];
