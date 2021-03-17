ef466f94fd006598bf939121d3771d835126657b
Adilet Yerkin <adilet.yerkin@gmail.com>
2021-03-17 12:55:55 +0600

    services add products

--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -2,16 +2,19 @@ export class Products{
   id: number;
   name: string;
   type: string;
+  typedict: string;
   description: string;
   img: string;
   byweightornot: string;
   more: string;
 
 
-constructor(id: number, name: string, type: string, description: string, img: string, byweightornot: string, more: string) {
+// tslint:disable-next-line:max-line-length
+constructor(id: number, name: string, type: string, typedict: string, byweightornot: string, description: string, img: string,  more: string) {
   this.id = id;
   this.name = name;
   this.type = type;
+  this.typedict = typedict;
   this.byweightornot = byweightornot;
   this.description = description;
   this.img = img;
