7b0f0946c4bfd11edc81d583ae99d289774935ac
Adilet Yerkin <adilet.yerkin@gmail.com>
2021-05-17 23:51:13 +0600

    history about

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, UrlTree} from '@angular/router';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  // logOut(): true | UrlTree {
+  //   this.authService.redirectUrl = url;
+  //   // Redirect to the login page
+  //   return this.router.parseUrl('ProductsComponent');
+  // }
+
 
 }
